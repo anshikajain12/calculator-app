1c9ab4d85b2b19f783e47db1c5a95138e1c4bdef
anshika jain <nisthajain2412@gmail.com>
2022-07-02 13:17:15 +0530

    updated

--- src/components/CalculatorApp.js
+++ src/components/CalculatorApp.js
@@ -3,7 +3,13 @@ import React from 'react'
 const CalculatorApp = () => {
   return (
     <>
-    <div>Hello</div>
+    <section>
+        <div className="bg-gray-900 w-96 h-screen mx-auto rounded-lg overflow-hidden mt-10">
+            <div>
+                
+            </div>
+        </div>
+    </section>
     </>
   )
 }
